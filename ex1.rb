0b4f2647c05831ec3a70db5223facf48a3a7d888
Irene Canuti <irene.canuti@gmail.com>
2016-11-28 13:03:51 +0000

    First commit. Exercise 1

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+# puts "Hello World!"
+puts "Hello Again"
+# puts "I like typing this."
+# puts "This is fun."
+# puts "Yay! Printing."
+# puts "I'd much rather you don't."
+# puts 'I "said" do not touch this.'
+# puts "Here another line!"
