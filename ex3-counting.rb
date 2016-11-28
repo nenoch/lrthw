64c15dc1af8de98d8b966efaf5deb55c4dfbc060
Irene Canuti <irene.canuti@gmail.com>
2016-11-28 13:35:56 +0000

    Exercise 3 + 1 extra

---
+++ ex3-counting.rb
@@ -0,0 +1,8 @@
+puts "I wonder how many days I will be on Makers Academy:"
+puts "I guess it is gonna be #{4*4*7} days roughly"
+puts "Even #{(4*4*7)+7*2} if I keep studying during Xmas holidays."
+puts "Well, I might take weekends off. So maybe #{(4*4*5)+5*2} days."
+puts "Thinking in hours, considering I am doing the 1st month part-time and then 12 weeks full-time I will study a total of #{(4*20)+(4*40)} hours."
+puts "So, will I need to commit more time next year?"
+puts 4*20 < 4*40
+puts "Mmmm I am sure I will use most of my #{(4*4*2)+2*2} weekend days too though..."
